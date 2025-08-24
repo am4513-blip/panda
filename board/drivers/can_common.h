6ccf43eb02921da72e29f1a6c1acad1447d55f9a
am4513-blip <am4513@protonmail.com>
2025-08-24 10:45:03 -0700

    set bus spd to 250kbps for CAN0/CAN2 of Body Panda

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -142,6 +142,16 @@ void can_init_all(void) {
     if (!current_board->has_canfd) {
       bus_config[i].can_data_speed = 0U;
     }
+    // If 3rd panda (2nd External panda), For CAN0 and CAN2 set to 250kbps for Body BUS communication
+    // Make sure the current board is the "Black" panda and not the internal Panda in the C3X
+    // The addresses where the unique ID is stored is different for the STM32F7 (Tres Panda) than the STM32F4 (Black Panda)
+    if(memcmp(current_board->board_type, hex_values, 0x05) == 0)
+    { // Panda uids for both car and bench
+      if( ((*uid0 == 0x003E000F) || (*uid0 == 0x001B0028)) && ((bus_config[i].bus_lookup == 0U) || (bus_config[i].bus_lookup == 2U))  )
+      {                              
+        bus_config[i].can_speed =  2500U;
+      }
+    }
     can_clear(can_queues[i]);
     (void)can_init(i);
   }
